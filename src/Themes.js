18325a39d619137f2f88a663f707d882805b5ccc
Cadell Singh <cadellsingh9@gmail.com>
2020-12-09 21:38:14 -0400

    added dark theme colors

---
+++ src/Themes.js
@@ -0,0 +1,11 @@
+export const darkTheme = {
+  body: "hsl(235, 21%, 11%)",
+  todoText: "hsl(234, 39%, 85%)",
+  clickedText: "hsl(220, 98%, 61%)",
+  text: "hsl(234, 11%, 52%)",
+  // filterContainerText: "hsl(234, 11%, 52%)",
+  // todoCompleted: "hsl(234, 11%, 52%)",
+  // footerText: "hsl(234, 11%, 52%)",
+};
+
+export const lightTheme = {};
